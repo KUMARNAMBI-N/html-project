f105eab4a8e98fafaf2ff42640edf38c7eadecd0
KUMARNAMBI-N <kumarnambi56@gmial.com>
2024-06-05 13:48:04 +0530

    multiple try

---
+++ Java script/script.js
@@ -0,0 +1,7 @@
+function mobileMenus(){
+    var menus = document.getElementById('horiMenu');
+    var crossMenus = document.getElementById('closeMenu');
+    menus.classList.toggle('open');
+    crossMenus.classList.toggle('open');
+
+}
